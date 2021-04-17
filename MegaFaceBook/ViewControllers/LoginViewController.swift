a8658738dfb6efe3252d6420c6f50d60f74994ab
Sergeytsev A <Sergeytsev A>
2021-04-17 18:13:15 +0300

    update login

---
+++ MegaFaceBook/ViewControllers/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  MegaFaceBook
+//
+//  Created by 18316409 on 17.04.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var nameTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
