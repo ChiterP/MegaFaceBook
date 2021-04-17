30ed5f6d80c570ba12b65a03479fce92eee78908
MatveenkovNikita <matveenkoff@gmail.com>
2021-04-17 19:27:08 +0300

    ver Robot

---
+++ MegaFaceBook/ViewControllers/RobotViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RobotViewController.swift
+//  MegaFaceBook
+//
+//  Created by Никита  on 17.04.2021.
+//
+
+import UIKit
+
+class RobotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
