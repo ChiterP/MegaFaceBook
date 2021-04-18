93e2c2469121589afe63440626c2f731a82bb614
MatveenkovNikita <matveenkoff@gmail.com>
2021-04-18 23:21:36 +0300

    robot 1.0

--- MegaFaceBook/ViewControllers/RobotViewController.swift
+++ MegaFaceBook/ViewControllers/RobotViewController.swift
@@ -11,6 +11,9 @@ class RobotViewController: UIViewController {
     
     @IBOutlet weak var labelUI: UILabel!
     
+    @IBOutlet var autoFirtstImage: UIImageView!
+    @IBOutlet var trainFirstIMAGE: UIImageView!
+    @IBOutlet var autoSecondImage: UIImageView!
     
     
 
@@ -20,15 +23,12 @@ class RobotViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func getTapImage(_ sender: Any) {
+        if let _ = sender as? UIImage {
+            view.backgroundColor = .red
+        }
     }
-    */
+    
+
 
 }
