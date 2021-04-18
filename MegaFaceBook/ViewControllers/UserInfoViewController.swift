be4e7619408e7660dcc26826dcbbd70543faeb11
Mikhail Mezentsev <mezya31@yandex.ru>
2021-04-18 03:19:34 +0300

    Created UserInfoVC and UserInfo layout.

---
+++ MegaFaceBook/ViewControllers/UserInfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserInfoViewController.swift
+//  MegaFaceBook
+//
+//  Created by Михаил Мезенцев on 18.04.2021.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
