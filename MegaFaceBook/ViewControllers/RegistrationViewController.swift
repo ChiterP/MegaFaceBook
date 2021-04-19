f9885eda4d07899b9fcc02fe96e726962c0462d5
Nikita Kelevra <nonamerz5@mail.ru>
2021-04-19 20:21:40 +0300

    Работа на Регистрационным VC / Создаю к нему класс - новый	 фаил

---
+++ MegaFaceBook/ViewControllers/RegistrationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegistrationViewController.swift
+//  MegaFaceBook
+//
+//  Created by Nikita Kelevra on 19.04.2021.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        
+    }
+    
+
+ 
+    
+
+}
