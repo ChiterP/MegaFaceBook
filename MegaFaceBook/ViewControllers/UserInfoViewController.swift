ce9d5bfd092bbba333c608a61b5d981774bdef86
Mikhail Mezentsev <mezya31@yandex.ru>
2021-04-20 00:45:40 +0300

    Создал новый файл для расширения view. В этом расширении создал метод для задания градиента фона view. Обновил UserInfoViewController.

--- MegaFaceBook/ViewControllers/UserInfoViewController.swift
+++ MegaFaceBook/ViewControllers/UserInfoViewController.swift
@@ -12,11 +12,41 @@ class UserInfoViewController: UIViewController {
     @IBOutlet weak var greetingLabel: UILabel!
     @IBOutlet weak var userInfoLabel: UILabel!
     
+    @IBOutlet weak var userImage: UIImageView!
+    
     var person: Person!
+    
+    private let primaryColor = UIColor(
+        red: 210 / 255,
+        green: 109 / 255,
+        blue: 128 / 255,
+        alpha: 1
+    )
+    
+    private let secondaryColor = UIColor(
+        red: 107 / 255,
+        green: 148 / 255,
+        blue: 230 / 255,
+        alpha: 1
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        greetingLabel.text = "Hello, \(person.user.name) \(person.user.surname)! 👻"
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        greetingLabel.text = "Hello, \(person.user.fullName)! 👻"
+        showGreeting(for: person)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        userImage.image = UIImage(named: person.user.image)
+    }
+    
+    private func showGreeting(for person: Person) {
+        if person.user.name == "Admin" {
+            userInfoLabel.text = "Don't brake anything!"
+        } else {
+            userInfoLabel.text = "We're sure you'll like our amazing app!"
+        }
     }
 }
