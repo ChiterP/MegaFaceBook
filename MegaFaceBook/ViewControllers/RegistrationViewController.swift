9aa488dec908b76809a3149fa178c841688b921b
Nikita Kelevra <nonamerz5@mail.ru>
2021-04-25 09:08:09 +0300

    Откорректировал цвет окна регистрации

--- MegaFaceBook/ViewControllers/RegistrationViewController.swift
+++ MegaFaceBook/ViewControllers/RegistrationViewController.swift
@@ -32,9 +32,23 @@ class RegistrationViewController: UIViewController {
     //private let person = Person.getPersonData()
     var regUser = User(login: "", password: "", email: "", person: Person(name: "", lastName: "", aboutUser: "", image: ""))
     
+    private let primaryColor = UIColor(
+        red: 210 / 255,
+        green: 109 / 255,
+        blue: 128 / 255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 107 / 255,
+        green: 148 / 255,
+        blue: 230 / 255,
+        alpha: 1
+    )
+    
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
         setupUI()
     }
     
